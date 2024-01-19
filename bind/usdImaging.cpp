45ec5d1de6a981ee885da4bdf62ad357cebe9f5f
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-01-20 00:42:29 +1300

    Add UsdImagingGLEngine::SetCameraState

--- bind/usdImaging.cpp
+++ bind/usdImaging.cpp
@@ -8,28 +8,21 @@
 #include <pxr/usd/usdGeom/camera.h>
 
 namespace bblext {
-    void set_camera(PXR_NS::UsdImagingGLEngine& engine, const PXR_NS::UsdPrim& cameraPrim) {
-        auto camera = pxr::UsdGeomCamera(cameraPrim).GetCamera(pxr::UsdTimeCode::Default());
-
-        engine.SetCameraState(camera.GetFrustum().ComputeViewMatrix(),
-            camera.GetFrustum().ComputeProjectionMatrix());
-    }
-
     void render(PXR_NS::UsdImagingGLEngine& engine, const PXR_NS::UsdPrim& prim) {
         pxr::UsdImagingGLRenderParams params;
         params.enableLighting = false;
         engine.Render(prim, params);
-
     }
 }
 
 BBL_MODULE(usdImaging) {
     bbl::fn(&bblext::render, "render");
-    bbl::fn(&bblext::set_camera, "set_camera");
 
     bbl::Class<PXR_NS::UsdImagingGLEngine>("GLEngine")
         .ctor(bbl::Class<PXR_NS::UsdImagingGLEngine>::Ctor<>(), "new")
-        .m(&PXR_NS::UsdImagingGLEngine::SetRenderViewport);
+        .m(&PXR_NS::UsdImagingGLEngine::SetRenderViewport)
+        .m(&PXR_NS::UsdImagingGLEngine::SetCameraState)
+        .m(&PXR_NS::UsdImagingGLEngine::Render);
 
 }
 
