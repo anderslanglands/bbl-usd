2c4a391a8ad15ebb1343deaf781b78683ec3ae88
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-01-30 15:24:51 +1300

    SetCullStyle

--- bind/usdImaging.cpp
+++ bind/usdImaging.cpp
@@ -9,6 +9,10 @@ namespace bblext {
     void GLRenderParams_SetEnableLighting(PXR_NS::UsdImagingGLRenderParams& params, bool enableLighting) {
         params.enableLighting = enableLighting;
     }
+
+    void GLRenderParams_SetCullStyle(PXR_NS::UsdImagingGLRenderParams& params, PXR_NS::UsdImagingGLCullStyle cullStyle) {
+        params.cullStyle = cullStyle;
+    }
 }
 
 BBL_MODULE(usdImaging) {
@@ -17,12 +21,17 @@ BBL_MODULE(usdImaging) {
         .m(&PXR_NS::UsdImagingGLEngine::SetRenderViewport)
         .m(&PXR_NS::UsdImagingGLEngine::SetCameraState)
         .m(&PXR_NS::UsdImagingGLEngine::Render)
-        .m(&PXR_NS::UsdImagingGLEngine::SetRendererSetting);
+        .m(&PXR_NS::UsdImagingGLEngine::SetRendererSetting)
+        .m(&PXR_NS::UsdImagingGLEngine::TestIntersection);
+    ;
 
     bbl::Class<PXR_NS::UsdImagingGLRenderParams>("GLRenderParams")
         .ctor(bbl::Class<PXR_NS::UsdImagingGLRenderParams>::Ctor<>(), "new");
 
-    bbl::fn(&bblext::GLRenderParams_SetEnableLighting, "GLRenderParams_SetEnableLighting");
+    bbl::Enum<PXR_NS::UsdImagingGLCullStyle>("GLCullStyle");
+
+    bbl::fn(&bblext::GLRenderParams_SetEnableLighting);
+    bbl::fn(&bblext::GLRenderParams_SetCullStyle);
 }
 
 
